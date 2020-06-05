06635eea3ce5b6f91fe03eef8b635a4cd8157a1c
cody clark <codyclark2119@gmail.com>
2020-06-05 14:11:33 -0500

    Base react setup and logical framework

---
+++ storefront/src/utils/actions.jsx
@@ -0,0 +1,10 @@
+export const CREATE_USER = "CREATE_USER";
+export const LOGIN_USER = "LOGIN_USER";
+export const UPDATE_USER = "UPDATE_USER";
+export const DELETE_USER = "DELETE_USER"
+export const ADD_CART = "ADD_CART"
+export const REMOVE_CART = "REMOVE_CART";
+export const CLEAR_CART = "CLEAR_CART"
+export const SAVE_ORDER = "SAVE_ORDER";
+export const CREATE_ADMIN = "CREATE_ADMIN";
+export const LOGIN_ADMIN = "LOGIN_ADMIN"
